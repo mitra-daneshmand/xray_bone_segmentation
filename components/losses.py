059ad6241ab2f90c17e994c373d32a71c29663f9
mitra-daneshmand <74309250+mitra-daneshmand@users.noreply.github.com>
2022-10-11 15:51:36 +0300

    Update losses.py

--- components/losses.py
+++ components/losses.py
@@ -1,11 +1,18 @@
 import logging
+from typing import List
 
-from torch import nn
-
+import torch
+from torch import nn, Tensor, einsum
 
 logging.basicConfig()
 logger = logging.getLogger('losses')
 logger.setLevel(logging.DEBUG)
+
+
+if torch.cuda.is_available():
+    maybe_gpu = 'cuda'
+else:
+    maybe_gpu = 'cpu'
 
 
 class CrossEntropyLoss(nn.Module):
@@ -33,7 +40,7 @@ class CrossEntropyLoss(nn.Module):
                        .format(repr(kwargs)))
         self.ce = nn.CrossEntropyLoss(weight=class_weight)
 
-    def forward(self, input_, target, **kwargs):
+    def forward(self, input, target):
         """
         Parameters
         ----------
@@ -43,5 +50,41 @@ class CrossEntropyLoss(nn.Module):
         -------
         out: float tensor
         """
-        return self.ce(input_, target)
+        return self.ce(input, target)
+
+
+class GeneralizedDice():
+    def __init__(self, **kwargs):
+        # Self.idc is used to filter out some classes of the target mask. Use fancy indexing
+        self.idc: List[int] = kwargs["idc"]
+
+    def __call__(self, probs: Tensor, target: Tensor) -> Tensor:
+        pc = probs[:, self.idc, ...].type(torch.float32)
+        tc = target[:, self.idc, ...].type(torch.float32)
+
+        w: Tensor = 1 / ((einsum("bkwh->bk", tc).type(torch.float32) + 1e-10) ** 2)
+        intersection: Tensor = w * einsum("bkwh,bkwh->bk", pc, tc)
+        union: Tensor = w * (einsum("bkwh->bk", pc) + einsum("bkwh->bk", tc))
+
+        divided: Tensor = 1 - 2 * (einsum("bk->b", intersection) + 1e-10) / (einsum("bk->b", union) + 1e-10)
+
+        loss = divided.mean()
+
+        return loss.to(maybe_gpu)
+
+
+class BoundaryLoss():
+    def __init__(self, **kwargs):
+        # Self.idc is used to filter out some classes of the target mask. Use fancy indexing
+        self.idc: List[int] = kwargs["idc"]
+
+    def __call__(self, probs: Tensor, dist_maps: Tensor) -> Tensor:
+        pc = probs[:, self.idc, ...].type(torch.float32)
+        dc = dist_maps[:, self.idc, ...].type(torch.float32)
+
+        multipled = einsum("bkwh,bkwh->bkwh", pc, dc)
+
+        loss = multipled.mean()
+
+        return loss.to(maybe_gpu)
 
