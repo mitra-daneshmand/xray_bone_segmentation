71e68aa181a711e0207cbb44d618559748ff2604
mitra-daneshmand <74309250+mitra-daneshmand@users.noreply.github.com>
2022-10-11 15:50:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import gc
-import os
 import logging
-import pandas as pd
-import numpy as np
-import torch
-from torch.utils.data import DataLoader
-from seed import seed
+import warnings
 from collections import defaultdict
-from dataset import DatasetOAIfor
-from preproc import custom, transforms
-from trainer import ModelTrainer
 
 import cv2
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
-import warnings
 
+from dataset import DatasetOAIfor
+from preproc import custom, trnsfrms
+from seed import seed
+from trainer import ModelTrainer
 
 cv2.ocl.setUseOpenCL(False)
 cv2.setNumThreads(0)
@@ -29,7 +26,7 @@ warnings.filterwarnings('ignore')
 def generate_ds(train_df, val_df):
     train_df['path_image'] = train_df['path_mask'] = val_df['path_image'] = val_df['path_mask'] = ''
 
-    train_df['path_image'] = 'data/imgs/ID_side.png'
+    train_df['path_image'] = 'data/prep/ID_side.png'
     train_df['path_image'] = train_df.apply(
         lambda train_df: train_df['path_image'].replace('ID', str(train_df['ID'])), axis=1)
     train_df['path_image'] = train_df.apply(
@@ -40,7 +37,7 @@ def generate_ds(train_df, val_df):
     train_df['path_mask'] = train_df.apply(
         lambda train_df: train_df['path_mask'].replace('side', train_df['SIDE']), axis=1)
 
-    val_df['path_image'] = 'data/imgs/ID_side.png'
+    val_df['path_image'] = 'data/prep/ID_side.png'
     val_df['path_image'] = val_df.apply(
         lambda val_df: val_df['path_image'].replace('ID', str(val_df['ID'])), axis=1)
     val_df['path_image'] = val_df.apply(
@@ -52,6 +49,7 @@ def generate_ds(train_df, val_df):
         lambda val_df: val_df['path_mask'].replace('side', val_df['SIDE']), axis=1)
 
     return train_df, val_df
+
 
 def estimate_mean_std(dataset):
     mean_std_loader = DataLoader(dataset, batch_size=16, num_workers=16, pin_memory=torch.cuda.is_available())
@@ -72,9 +70,6 @@ def estimate_mean_std(dataset):
 
 
 batch_size = 16
-H = 512
-W = 512
-
 fold_scores = dict()
 name_ds = 'OAI'
 data = pd.read_csv('train_data.csv')
@@ -89,52 +84,36 @@ for fold_num in range(5):
 
     datasets = defaultdict(dict)
 
-    if not os.path.exists(os.path.join('sessions/mean/', 'mean_fold_{}.pth'.format(fold_num))):
-        datasets[name_ds]['train'] = DatasetOAIfor(df_meta=train_set)
-        print('Calculating mean and std for normalization:')
-        mean, std = estimate_mean_std(datasets[name_ds]['train'])
-
-        print("Saving mean and std files!")
-        torch.save(mean, os.path.join('sessions/mean/', 'mean_fold_{}.pth'.format(fold_num)))
-        torch.save(std, os.path.join('sessions/std/', 'std_fold_{}.pth'.format(fold_num)))
-        print("Normalization done!")
-    else:
-        print('Loading mean and std for normalization:')
-        mean = torch.load(os.path.join('sessions/mean/', 'mean_fold_{}.pth'.format(fold_num)))
-        std = torch.load(os.path.join('sessions/std/', 'std_fold_{}.pth'.format(fold_num)))
+    mean = 90.3174
+    std = 83.7574
 
     datasets[name_ds]['train'] = DatasetOAIfor(
         df_meta=train_set,
         transforms=[
-            # custom.PercentileClippingAndToFloat(cut_min=10, cut_max=30),
-            transforms.CenterCrop(height=355, width=215),
-            # transforms.HorizontalFlip(prob=.5),
-            # transforms.GammaCorrection(gamma_range=(0.5, 1.5), prob=.5),
-            transforms.OneOf([
-                transforms.DualCompose([
-                    transforms.Scale(ratio_range=(0.7, 0.8), prob=1.),
-                    transforms.Scale(ratio_range=(1.5, 1.6), prob=1.),
+            trnsfrms.HorizontalFlip(prob=.5),
+            # trnsfrms.GammaCorrection(gamma_range=(0.5, 1.5), prob=.5),
+            trnsfrms.OneOf([
+                trnsfrms.DualCompose([
+                    trnsfrms.Scale(ratio_range=(0.7, 0.8), prob=1.),
+                    trnsfrms.Scale(ratio_range=(1.5, 1.6), prob=1.),
                 ]),
-                transforms.NoTransform()
+                trnsfrms.NoTransform()
             ]),
-            transforms.Crop(output_size=(355, 215)),
-            # transforms.BilateralFilter(d=5, sigma_color=50, sigma_space=50, prob=.3),
+            trnsfrms.Crop(output_size=(385, 245)),
             custom.Normalize(mean=mean, std=std),
-            transforms.ToTensor(),
-        ])
+            trnsfrms.ToTensor()]
+        )
     datasets[name_ds]['val'] = DatasetOAIfor(
         df_meta=val_set,
         transforms=[
-            # custom.PercentileClippingAndToFloat(cut_min=10, cut_max=30),
-            transforms.CenterCrop(height=355, width=215),
+            trnsfrms.CenterCrop(height=385, width=245),
             custom.Normalize(mean=mean, std=std),
-            transforms.ToTensor()
-        ])
+            trnsfrms.ToTensor()]
+        )
 
     loaders = defaultdict(dict)
-    loaders[name_ds]['train'] = DataLoader(datasets[name_ds]['train'], batch_size=batch_size, shuffle=True,
-                                           num_workers=8,
-                                           pin_memory=torch.cuda.is_available())
+    loaders[name_ds]['train'] = DataLoader(datasets[name_ds]['train'], batch_size=batch_size, shuffle=False,
+                                           num_workers=8, pin_memory=torch.cuda.is_available())
     loaders[name_ds]['val'] = DataLoader(datasets[name_ds]['val'], batch_size=batch_size, shuffle=False, num_workers=8,
                                          pin_memory=torch.cuda.is_available())
 
