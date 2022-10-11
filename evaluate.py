26194e679906167507866b8675e137b404f806f5
mitra-daneshmand <74309250+mitra-daneshmand@users.noreply.github.com>
2022-10-11 15:45:36 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,25 +1,21 @@
-import os
 import torch
 from torch.utils.data import DataLoader
-from collections import defaultdict
 import pandas as pd
-import numpy as np
-from tqdm import tqdm
 from dataset import DatasetOAIfor
-from preproc import custom, transforms
+from preproc import custom, trnsfrms
 from evaluation_tools import predict_folds, merge_predictions
 
 
 def generate_ds(df):
     df['path_image'] = df['path_mask'] = ''
 
-    df['path_image'] = 'data/imgs/ID_side.png'
+    df['path_image'] = 'data/prep/ID_side.png'
     df['path_image'] = df.apply(
         lambda df: df['path_image'].replace('ID', str(df['ID'])), axis=1)
     df['path_image'] = df.apply(
         lambda df: df['path_image'].replace('side', df['SIDE']), axis=1)
 
-    df['path_mask'] = 'data/imgs/ID_side.png'
+    df['path_mask'] = 'data/prep/ID_side.png'
     df['path_mask'] = df.apply(
         lambda df: df['path_mask'].replace('ID', str(df['ID'])), axis=1)
     df['path_mask'] = df.apply(
@@ -28,33 +24,33 @@ def generate_ds(df):
     return df
 
 
-
-batch_size = 16
+batch_size = 32
 test_set = pd.read_csv('test_data.csv')
 test_set = generate_ds(test_set)
 
 fold_idcs = list(range(5))
 
-mean, std = 8.32237651, 6.92832947
-
+# mean, std = 0, 1
+mean = 90.3174
+std = 83.7574
 
 dataset_test = DatasetOAIfor(
         df_meta=test_set,
         transforms=[
             # custom.PercentileClippingAndToFloat(cut_min=10, cut_max=30),
-            transforms.CenterCrop(height=355, width=215),
+            # trnsfrms.CenterCrop(height=355, width=215),
             custom.Normalize(mean=mean, std=std),
-            transforms.ToTensor()
-        ])
+            trnsfrms.ToTensor()]
+        )
 loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=8,
                                          pin_memory=torch.cuda.is_available())
 
-folds_preds = True
-merge_preds = False
+folds_preds = False
+merge_preds = True
 with torch.no_grad():
     if folds_preds:
         predict_folds(loader=loader_test, fold_idcs=fold_idcs)
 
     if merge_preds:
-        merge_predictions(loader=loader_test, save_plots=True, remove_foldw=False)
+        merge_predictions(loader=loader_test, save_plots=True)
 
