9fd53a170af699cfb79d76a0275aaa4d4cfd31d6
mitra-daneshmand <74309250+mitra-daneshmand@users.noreply.github.com>
2024-04-27 14:51:40 +0300

    Add files via upload

---
+++ components/utils.py
@@ -0,0 +1,103 @@
+from functools import partial
+from typing import Iterable, Set, Tuple, TypeVar, cast
+
+import numpy as np
+import torch
+import torch.sparse
+# from medpy.metric.binary import hd
+from scipy.ndimage import distance_transform_edt as eucl_distance
+from torch import Tensor
+from tqdm import tqdm
+
+colors = ["c", "r", "g", "b", "m", 'y', 'k', 'chartreuse', 'coral', 'gold', 'lavender',
+          'silver', 'tan', 'teal', 'wheat', 'orchid', 'orange', 'tomato']
+
+# functions redefinitions
+tqdm_ = partial(tqdm, dynamic_ncols=True,
+                leave=False,
+                bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [' '{rate_fmt}{postfix}]')
+
+A = TypeVar("A")
+B = TypeVar("B")
+T = TypeVar("T", Tensor, np.ndarray)
+
+
+# Assert utils
+def uniq(a: Tensor) -> Set:
+    return set(torch.unique(a.cpu()).numpy())
+
+
+def sset(a: Tensor, sub: Iterable) -> bool:
+    return uniq(a).issubset(sub)
+
+
+def simplex(t: Tensor, axis=1) -> bool:
+    _sum = cast(Tensor, t.sum(axis).type(torch.float32))
+    _ones = torch.ones_like(_sum, dtype=torch.float32)
+    return torch.allclose(_sum, _ones)
+
+
+# switch between representations
+def probs2class(probs: Tensor) -> Tensor:
+    b, _, *img_shape = probs.shape
+    assert simplex(probs)
+
+    res = probs.argmax(dim=1)
+    assert res.shape == (b, *img_shape)
+
+    return res
+
+
+def one_hot(t: Tensor, axis=1) -> bool:
+    return simplex(t, axis) and sset(t, [0, 1])
+
+
+def class2one_hot(seg: Tensor, K: int) -> Tensor:
+    # Breaking change but otherwise can't deal with both 2d and 3d
+    # if len(seg.shape) == 3:  # Only w, h, d, used by the dataloader
+    #     return class2one_hot(seg.unsqueeze(dim=0), K)[0]
+
+
+    # print()
+    assert sset(seg, list(range(K))), (uniq(seg), K)
+
+    b, *img_shape = seg.shape  # type: Tuple[int, ...]
+
+    device = seg.device
+    res = torch.zeros((b, K, *img_shape), dtype=torch.int32, device=device).scatter_(1, seg[:, None, ...], 1)
+
+    assert res.shape == (b, K, *img_shape)
+    assert one_hot(res)
+
+    return res
+
+
+def probs2one_hot(probs: Tensor) -> Tensor:
+    _, K, *_ = probs.shape
+    assert simplex(probs)
+
+    res = class2one_hot(probs2class(probs), K)
+    assert res.shape == probs.shape
+    assert one_hot(res)
+
+    return res
+
+
+def one_hot2dist(seg: np.ndarray, resolution: Tuple[float, float, float] = None,
+                 dtype=None) -> np.ndarray:
+    assert one_hot(torch.tensor(seg), axis=0)
+    K: int = len(seg)
+
+    res = np.zeros_like(seg, dtype=dtype)
+    for k in range(K):
+        posmask = seg[k].astype(np.bool)
+
+        if posmask.any():
+            negmask = ~posmask
+            res[k] = eucl_distance(negmask, sampling=resolution) * negmask \
+                - (eucl_distance(posmask, sampling=resolution) - 1) * posmask
+        # The idea is to leave blank the negative classes
+        # since this is one-hot encoded, another class will supervise that pixel
+
+    return res
+
